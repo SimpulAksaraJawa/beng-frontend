11ea9a2c33ba23651bd93caca9554f54b5633e1b
jesselyn <jesselynva@gmail.com>
2025-09-26 23:53:53 +0700

    chore: setup shadcn + tweak + foldering +routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from "path"
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import generouted from "@generouted/react-router/plugin";
@@ -6,4 +7,9 @@ import generouted from "@generouted/react-router/plugin";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), generouted()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
