1900663da07c13a68ae0ea52fdb6d2fc8da9a0ba
G30TR1X <gavriel10donovan@gmail.com>
2025-09-28 13:46:36 +0700

    Login Page, Login Page, Login Page

--- src/router.ts
+++ src/router.ts
@@ -1,22 +1,25 @@
 // Generouted, changes to this file will be overridden
 /* eslint-disable */
 
-import { components, hooks, utils } from '@generouted/react-router/client'
+import { components, hooks, utils } from "@generouted/react-router/client";
 
 export type Path =
-  | `/`
-  | `/customer`
-  | `/dashboard`
-  | `/product`
-  | `/stock`
-  | `/supplier`
+    | `/`
+    | `/customer`
+    | `/dashboard`
+    | `/login`
+    | `/product`
+    | `/stock`
+    | `/supplier`;
 
-export type Params = {
-  
-}
+export type Params = {};
 
-export type ModalPath = never
+export type ModalPath = never;
 
-export const { Link, Navigate } = components<Path, Params>()
-export const { useModals, useNavigate, useParams } = hooks<Path, Params, ModalPath>()
-export const { redirect } = utils<Path, Params>()
+export const { Link, Navigate } = components<Path, Params>();
+export const { useModals, useNavigate, useParams } = hooks<
+    Path,
+    Params,
+    ModalPath
+>();
+export const { redirect } = utils<Path, Params>();
