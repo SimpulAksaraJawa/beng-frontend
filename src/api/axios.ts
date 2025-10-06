36932f05cc1e629fe5e1a03d43068873877ed4fc
Jesselyn <120245576+jesselynevy@users.noreply.github.com>
2025-10-06 09:41:28 +0700

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const BASE_URL =
     import.meta.env.MODE === "development"
-        ? "http://localhost:5000/api" // Might want to change port from 3000 to your desired port
-        : "/api";
+        ? "http://localhost:5000/api"
+        : "http://167.71.195.190/api";
 
 const api = axios.create({
     baseURL: BASE_URL,
