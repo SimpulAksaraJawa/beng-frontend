f4770ddfd2bf17c44f7251f5606ecdab8eddc561
Jesselyn <120245576+jesselynevy@users.noreply.github.com>
2025-10-06 18:05:12 +0700

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
     import.meta.env.MODE === "development"
         ? "http://localhost:5000/api"
-        : "https://bengbeng.web.id/api"; //okay
+        : "https://api.bengbeng.web.id/api"; //okay
 
 const api = axios.create({
     baseURL: BASE_URL,
