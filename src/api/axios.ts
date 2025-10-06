7f3ffdaaa0464dea912850f87aad7ac2f707c728
Deswandy <162786961+Deswandy@users.noreply.github.com>
2025-10-06 11:23:26 +0700

    bisaaa

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
     import.meta.env.MODE === "development"
         ? "http://localhost:5000/api"
-        : "http://167.71.195.190/api";
+        : "https://bengbeng.web.id/api";
 
 const api = axios.create({
     baseURL: BASE_URL,
